fdf5720c9be9c791c9d95ccf5344c5d457d4e4c4
Railson Rames <rames.cloud@outlook.com.br>
2019-07-31 17:14:17 -0300

    Add project files.

---
+++ Challenge.Api/Controllers/RecordsController.cs
@@ -0,0 +1,52 @@
+﻿using Challenge.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Challenge.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("list")]
+        public ActionResult<IEnumerable<RecordObjectModel>> GetRecords()
+        {
+            return new List<RecordObjectModel> {
+                new RecordObjectModel{
+                    Id = 1,
+                    Name = "Railson Rames",
+                    Status = true
+                },
+                new RecordObjectModel{
+                    Id = 2,
+                    Name = "Rayanne de Brito Uchoa",
+                    Status = true
+                },
+                new RecordObjectModel{
+                    Id = 3,
+                    Name = "Rayanninha Uchoa Rames",
+                    Status = true
+                }
+            };
+        }
+
+        [HttpPost]
+        [Route("save")]
+        public IActionResult PostRecords([FromBody]List<RecordObjectModel> jsonReceived)
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IActionResult Rock()
+        {
+            return Ok();
+        }
+    }
+
+}
---
+++ Challenge.Api/Models/RecordObjectModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge.Api.Models
+{
+    public class RecordObjectModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
