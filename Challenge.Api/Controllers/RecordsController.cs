aef4dac095c1071ad16cac74b6178a3899acaf55
Railson Rames <rames.cloud@outlook.com.br>
2019-08-01 18:06:55 -0300

    Primeira tentativa de gravação em arquivo CSV.

--- Challenge.Api/Controllers/RecordsController.cs
+++ Challenge.Api/Controllers/RecordsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace Challenge.Api.Controllers
 {
@@ -38,9 +40,23 @@ namespace Challenge.Api.Controllers
         [Route("save")]
         public IActionResult PostRecords([FromBody]List<RecordObjectModel> jsonReceived)
         {
+            string filePath = "data.csv";
             if (ModelState.IsValid)
             {
-                //to do
+                try
+                {
+                    using (StreamWriter file = new StreamWriter(filePath, true))
+                    {
+                        foreach (var record in jsonReceived)
+                        {
+                            file.WriteLine($"{record.Id},{record.Name},{record.Status}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new ApplicationException("Não foi possível gravar o objeto em arquivo, causado por: ", ex);
+                }
                 return Ok();
             }
             else
